3a946674224097b16074cb7b90b00147904cd009
gourdo1 <gourdo1@users.noreply.github.com>
2022-06-28 01:56:12 -0700

    user_config cosmetics

--- users/gourdo1/gourdo1.c
+++ users/gourdo1/gourdo1.c
@@ -138,33 +138,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t * record) {
         // User configuration toggles
     case PRNCONF:  // Print verbose status of all user_config toggles (open a text editor before engaging!!)
         if (record->event.pressed) {
-            SEND_STRING("GMMK Pro user configuration below. Press [FN]-<number key> to toggle settings."SS_TAP(X_ENT));
-            SEND_STRING("Config also visible by pressing [FN] and viewing keys 1 through 6."SS_TAP(X_ENT));
+            SEND_STRING(SS_TAP(X_ENT)"GMMK Pro user configuration below. Press [FN]+<number key> to toggle settings."SS_TAP(X_ENT));
+            SEND_STRING("Config also visible by holding [FN] and viewing keys 1 through 6."SS_TAP(X_ENT));
             SEND_STRING("=============================================================================="SS_TAP(X_ENT));
             if (user_config.rgb_hilite_caps) {
-                SEND_STRING("1. CapsLock RGB alpha keys ON"SS_TAP(X_ENT));
+                SEND_STRING("1. CapsLock RGB alpha keys                          [ON]"SS_TAP(X_ENT));
             } else {
-                SEND_STRING("1. CapsLock RGB alpha keys OFF"SS_TAP(X_ENT));
+                SEND_STRING("1. CapsLock RGB alpha keys                          [OFF]"SS_TAP(X_ENT));
             }
             if (user_config.rgb_hilite_numpad) {
-                SEND_STRING("2. Numpad RGB highlight ON"SS_TAP(X_ENT));
+                SEND_STRING("2. Numpad RGB highlight                             [ON]"SS_TAP(X_ENT));
             } else {
-                SEND_STRING("2. Numpad RGB highlight OFF"SS_TAP(X_ENT));
+                SEND_STRING("2. Numpad RGB highlight                             [OFF]"SS_TAP(X_ENT));
             }
             if (user_config.esc_double_tap_to_baselyr) {
-                SEND_STRING("3. Double tap ESC for _BASE layer ON"SS_TAP(X_ENT));
+                SEND_STRING("3. Double tap ESC for _BASE layer                   [ON]"SS_TAP(X_ENT));
             } else {
-                SEND_STRING("3. Double tap ESC for _BASE layer OFF"SS_TAP(X_ENT));
+                SEND_STRING("3. Double tap ESC for _BASE layer                   [OFF]"SS_TAP(X_ENT));
             }
             if (user_config.del_right_home_top) {
-                SEND_STRING("4. DEL right of BKSPC, HOME above (default)"SS_TAP(X_ENT));
+                SEND_STRING("4. DEL right of BKSPC, HOME above                   [DEFAULT]"SS_TAP(X_ENT));
             } else {
-                SEND_STRING("4. HOME right of BKSPC, DEL above (alternate)"SS_TAP(X_ENT));
+                SEND_STRING("4. HOME right of BKSPC, DEL above                   [ALTERNATE]"SS_TAP(X_ENT));
             }
             if (user_config.double_tap_shift_for_capslock) {
-                SEND_STRING("5. Capslock: Double tap LShift (Numpad on CapsLock key)"SS_TAP(X_ENT));
+                SEND_STRING("5. Capslock: Double tap LShift / Numpad on CapsLock [ON]"SS_TAP(X_ENT));
             } else {
-                SEND_STRING("5. Capslock: Standard key (Numpad on CapsLock OFF)"SS_TAP(X_ENT));
+                SEND_STRING("5. Capslock: Double tap LShift / Numpad on CapsLock [OFF]"SS_TAP(X_ENT));
             }
             if (user_config.encoder_press_mute_or_media) {
                 SEND_STRING("6. Encoder button mutes"SS_TAP(X_ENT));
